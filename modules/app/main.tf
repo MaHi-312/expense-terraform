9159512df24e5e4e4527e729321335ee63f97366
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-10-03 20:54:36 +0530

    expense scripts

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -32,7 +32,7 @@ resource "aws_launch_template" "template" {
 
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.security_group.id]
+  vpc_security_group_ids = [aws_security_group.security-group.id]
 
   tag_specifications {
     resource_type = "instance"
